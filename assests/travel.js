fdb865da40932b5aabe23c8614ed4e7b0a3a6d33
Stroupe79 <cstroupe79@gmail.com>
2019-04-04 15:03:11 -0500

    Added table, cdn dependinces

---
+++ assests/travel.js
@@ -0,0 +1,17 @@
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDBdToZozb2IWnhgukxR-HbYONqRhfz0gc",
+    authDomain: "transportation-schedule-60bd4.firebaseapp.com",
+    databaseURL: "https://transportation-schedule-60bd4.firebaseio.com",
+    projectId: "transportation-schedule-60bd4",
+    storageBucket: "transportation-schedule-60bd4.appspot.com",
+    messagingSenderId: "133777690038"
+};
+firebase.initializeApp(config);
+
+$.LoadingOverlaySetup({
+    background : "rgba(243, 245, 259, .75)",
+    imageColor : "#3880ff"
+  });
+  
