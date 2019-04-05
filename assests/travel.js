58a610572e3c9c8fab3adf82da35f168d7a40530
Stroupe79 <cstroupe79@gmail.com>
2019-04-05 16:25:18 -0500

    added db, added forms, writing to db

--- assests/travel.js
+++ assests/travel.js
@@ -1,5 +1,9 @@
 
  // Initialize Firebase
+
+ $(document).ready(function (){
+
+
  var config = {
     apiKey: "AIzaSyDBdToZozb2IWnhgukxR-HbYONqRhfz0gc",
     authDomain: "transportation-schedule-60bd4.firebaseapp.com",
@@ -10,3 +14,32 @@
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+const transport = ["Airship", "Bus", "Airplane", "Helicopter", "Submarine", "Ship"]
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    var dropdown = $("option:selected").val();
+    var dest = $("#dest").val();
+    var depart = $("#firstDep").val();
+    var freq = $("#freqDep").val()
+    
+    let dbAdd = {
+        mode: dropdown,
+        dest: dest,
+        time: depart,
+        frequency: freq,
+    }
+
+    database.ref().push(dbAdd)
+    tableAdd(dbAdd);
+    });
+
+function tableAdd(data){
+    console.log(data)
+
+    $("#newData").html("<tr><td id='data'>" + data.mode + "</td><td id='data'>" + data.dest + "</td><td id='data'>" + data.time + "</td><td id='data'>" + data.frequency + "</td></tr>");
+}
+
+});
